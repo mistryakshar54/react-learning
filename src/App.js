a1fd41f51ab4498cc66e31f4a8c2658e93b9f309
Akshar Mistry <mistry.aks@gmail.com>
2019-07-14 14:34:02 +0530

    Initaial Project Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ItemListComponent from './RenderProps/Components/ItemListComponent';
+import SummaryComponent from './RenderProps/Components/SummaryComponent';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1 className="header-label">Render Props Example</h1>
+        <ItemListComponent
+          render = {
+            (cartItems) => <SummaryComponent cartItems={cartItems} />
+          }
+        
+        />
     </div>
   );
 }
