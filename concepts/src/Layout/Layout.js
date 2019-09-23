cce0a9fcb005f03b963a723a26bf5764f434e70d
Akshar Mistry <mistry.aks@gmail.com>
2019-09-23 22:23:56 +0530

    Update Hooks section

--- concepts/src/Layout/Layout.js
+++ concepts/src/Layout/Layout.js
@@ -3,7 +3,7 @@ import { Route } from "react-router-dom";
 
 import ItemListLayout from "../RenderProps/Components/ItemListLayout";
 import Posts from "../HOC/Posts";
-import TimerHook from '../Hooks/Timer';
+import IngredientsListComponent from "../Hooks/Components/IngredientsList/IngredientsList";
 import ContextLayoutComponent from "../Context/Components/Layout";
 import PostsComponent from "../Thunk-Testing/PostsComponent";
 const LayoutComponent = () => {
@@ -11,7 +11,7 @@ const LayoutComponent = () => {
       <React.Fragment>
         <Route path="/" exact component={ItemListLayout} />
         <Route path="/HOC" exact component={Posts} />
-        <Route path="/Hook" exact component={TimerHook} />
+        <Route path="/Hook" exact component={IngredientsListComponent} />
         <Route path="/Context" exact component={ContextLayoutComponent} />
         <Route path="/Posts" exact component={PostsComponent} />
       </React.Fragment>
