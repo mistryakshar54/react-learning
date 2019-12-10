393e895de3a68848b4b523e6775773603fc09a3f
Akshar Mistry <mistry.aks@gmail.com>
2019-12-11 01:15:45 +0530

    Add Overlay component to the layout

--- mini-projects/video-player/src/components/VideoPlayerLayoutComponent/VideoPlayerLayout.js
+++ mini-projects/video-player/src/components/VideoPlayerLayoutComponent/VideoPlayerLayout.js
@@ -1,6 +1,7 @@
 import React , {Component} from 'react';
 import VideoPlayerComponent  from "./VideoPlayerComponent/VideoPlayerComponent";
 import PlaybackComponent from './PlayerControlsComponent/PlaybackComponent/PlaybackComponent';
+import VideoOverlayComponent from './VideoOverlayComponent/VideoOverlayComponent';
 import './VideoPlayerLayout.css';
 
 const videoPlayerRef = React.createRef();
@@ -16,7 +17,10 @@ class VideoPlayerLayout extends Component{
         return (
           <div>
             <VideoPlayerComponent ref={videoPlayerRef} />
-            <div className='videoControls showControls'>
+            <div className="videoOverlay">
+              <VideoOverlayComponent className="videoOverlay" isPlayerRunning={this.state.isPlayerRunning} />
+            </div>
+            <div className="videoControls showControls">
               <PlaybackComponent
                 videoRef={videoPlayerRef}
                 isPlayerRunning={this.state.isPlayerRunning}
