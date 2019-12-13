770799d20541cf5f76bd4c0867bb2c6a62255f56
Akshar Mistry <mistry.aks@gmail.com>
2019-12-13 23:02:19 +0530

    Update UI

--- mini-projects/video-player/src/components/VideoPlayerLayoutComponent/VideoPlayerComponent/VideoPlayerComponent.js
+++ mini-projects/video-player/src/components/VideoPlayerLayoutComponent/VideoPlayerComponent/VideoPlayerComponent.js
@@ -1,8 +1,7 @@
 import React from "react";
 
 const VideoPlayerComponent = React.forwardRef((props, ref) => {
-  let vidsrc =
-    "https://s3.amazonaws.com/codecademy-content/courses/React/react_video-fast.mp4";
+  let vidsrc = process.env.PUBLIC_URL + "vid.mp4";
 return <div className="videoPlayerComponent">
     <video ref={ref} src={vidsrc} ></video>
   </div>
