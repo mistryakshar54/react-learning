3eec6f4bff59db712bb7866c076cd8413429ce12
Akshar Mistry <mistry.aks@gmail.com>
2019-12-15 22:07:11 +0530

    Add animation for playing and pausing video

--- mini-projects/video-player/src/components/VideoPlayerLayoutComponent/PlayerControlsComponent/VideoOverlayComponent/VideoOverlayComponent.js
+++ mini-projects/video-player/src/components/VideoPlayerLayoutComponent/PlayerControlsComponent/VideoOverlayComponent/VideoOverlayComponent.js
@@ -1,29 +1,42 @@
-import React from 'react';
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlay , faPause } from "@fortawesome/free-solid-svg-icons";
-import PropTypes from 'prop-types'; // ES6
+import { faPlay, faPause } from "@fortawesome/free-solid-svg-icons";
+import PropTypes from "prop-types"; // ES6
+import { Transition } from "react-transition-group";
 
-const VideoOverlayComponent = (props) => {
-
-    const {isPlayerRunning} = props;
-
-    if( isPlayerRunning){
-        return (
-          <div className="overlayInfo">
-            <FontAwesomeIcon icon={faPause} />
-          </div>
-        );
-    }
-    else{
-        return (
-          <div className="overlayInfo">
-            <FontAwesomeIcon icon={faPlay} />
-          </div>
-        );
-    }
-}
+const VideoOverlayComponent = props => {
+  const { isPlayerRunning } = props;
+  return (
+    <div className="overlayInfo">
+      <Transition in={isPlayerRunning} timeout={500}>
+        {state => (
+          <React.Fragment>
+            <div
+              className="overlayInfo"
+              style={{
+                visibility: state === "entering" ? "visible" : "none",
+                opacity: state === "entering" ? 1 : 0
+              }}
+            >
+              <FontAwesomeIcon icon={faPlay} />
+            </div>
+            <div
+              className="overlayInfo"
+              style={{
+                visibility: state === "exiting" ? "visible" : "none",
+                opacity: state === "exiting" ? 1 : 0
+              }}
+            >
+              <FontAwesomeIcon icon={faPause} />
+            </div>
+          </React.Fragment>
+        )}
+      </Transition>
+    </div>
+  );
+};
 export default VideoOverlayComponent;
 
 VideoOverlayComponent.propTypes = {
-  isPlayerRunning : PropTypes.bool
-}
+  isPlayerRunning: PropTypes.bool
+};
