f126a8ee5ca1cd9dab5d67c99f663a1a1df1e173
Akshar Mistry <mistry.aks@gmail.com>
2019-12-26 12:59:38 +0530

    Update CountdownComponent.js

--- mini-projects/count-down-timer/src/components/CountdownComponent/CountdownComponent.js
+++ mini-projects/count-down-timer/src/components/CountdownComponent/CountdownComponent.js
@@ -1,13 +1,30 @@
 import React , {useState} from 'react';
 import TimerComponent from "./TimerComponent/TimerComponent";
+import InputComponent from "./InputComponent/InputComponent";
 const CountdownComponent = ( props ) => {
-
+    const [ isTimerRunning , setTimerRunning ] = useState(false);
     const timerValue = {
         hours : 0,
-        minutes: 2,
-        seconds: 10
+        minutes: 0,
+        seconds: 0
+    }
+    const startCountdown = ( timerData ) => {
+        setTimer(timerData);
+        setTimerRunning(!isTimerRunning);
+    } 
+    const stopCountdown = () => {
+        setTimer(timerValue);
+        setTimerRunning(!isTimerRunning);
     }
     const [ timer , setTimer ] = useState(timerValue);
-    return <TimerComponent timerData={timer} />;
+    return (
+      <div>
+        {isTimerRunning ? (
+          <TimerComponent timerData={timer} stopCountdown={stopCountdown} />
+        ) : (
+          <InputComponent startCountdown={startCountdown} />
+        )}
+      </div>
+    );
 }
 export default CountdownComponent;
