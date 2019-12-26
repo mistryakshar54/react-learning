71621c13452bc963a66c1be9ad97fc753c4f45f8
Akshar Mistry <mistry.aks@gmail.com>
2019-12-26 21:38:47 +0530

    Add CSS for Setting Timer Component

--- mini-projects/count-down-timer/src/components/CountdownComponent/InputComponent/InputComponent.js
+++ mini-projects/count-down-timer/src/components/CountdownComponent/InputComponent/InputComponent.js
@@ -1,75 +1,62 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "./InputComponent.css";
-const InputComponent = ( props ) => {
-    const [ hours , setHours ] = useState(0);
-    const [ minutes, setMinutes] = useState(0);
-    const [ seconds, setSeconds] = useState(0);
-    const startTimer = () => {
-        props.startCountdown({
-            hours,minutes,seconds
-        });
-    }
-    return (
-      <div className="timer-component">
-        <div className="timer-component">
-          <p className="time-component">
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlay, faStar } from "@fortawesome/free-solid-svg-icons";
+ 
+const InputComponent = props => {
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  const startTimer = () => {
+    props.startCountdown({
+      hours,
+      minutes,
+      seconds
+    });
+  };
+  return (
+    <div className="input-component-container">
+      <div className="input-component">
+        <p className="time-component">
+          <div className="input-container">
             <input
               className="timer-input"
               type="number"
               value={hours}
               onChange={e => setHours(e.target.value)}
             />
-            <span>Hours</span>
-          </p>
-          <p className="time-component">
+          </div>
+          <span>Hours</span>
+        </p>
+        <p className="time-component">
+          <div className="input-container">
             <input
               className="timer-input"
               type="number"
               value={minutes}
               onChange={e => setMinutes(e.target.value)}
             />
-            <span>Minutes</span>
-          </p>
-          <p className="time-component">
+          </div>
+          <span>Minutes</span>
+        </p>
+        <p className="time-component">
+          <div className="input-container">
             <input
               className="timer-input"
               type="number"
               value={seconds}
               onChange={e => setSeconds(e.target.value)}
             />
-            <span>Seconds</span>
-          </p>
-        </div>
-        <div>
-          <button onClick={startTimer}> Start </button>
-        </div>
-
-        {/* <div className="inputContainers">
-          <input
-            className="timer-input"
-            type="number"
-            value={hours}
-            onChange={e => setHours(e.target.value)}
-          />
-          :
-          <input
-            className="timer-input"
-            type="number"
-            value={minutes}
-            onChange={e => setMinutes(e.target.value)}
-          />
-          :
-          <input
-            className="timer-input"
-            type="number"
-            value={seconds}
-            onChange={e => setSeconds(e.target.value)}
-          />
-        </div>
-        <div>
-          <button onClick={startTimer}> Start </button>
-        </div> */}
+          </div>
+          <span>Seconds</span>
+        </p>
       </div>
-    );
-}
+      <div id="play-btn">
+        <div onClick={startTimer}>
+          <FontAwesomeIcon id="play-icon" icon={faPlay} />
+        </div>
+      </div>
+    </div>
+  );
+};
 export default InputComponent;
