6a12bb2c0b63db306712308f13ee5ccbbbd8f6d8
Akshar Mistry <mistry.aks@gmail.com>
2019-12-28 22:50:17 +0530

    Hacker news clone

---
+++ mini-projects/hacker-news-clone-pwa/src/App.js
@@ -0,0 +1,41 @@
+import React , {Fragment} from "react";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import AppBar from "@material-ui/core/AppBar";
+import Link from "@material-ui/core/Link";
+import Toolbar from "@material-ui/core/Toolbar";
+
+import Layout from './Components/LayoutComponent/Layout';
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+function App() {
+  return (
+    <Fragment>
+      <AppBar position="relative">
+        <Toolbar>
+          <Typography variant="h6" color="inherit" noWrap>
+            Hacker News Clone
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Container maxWidth="xl">
+        <Layout />
+        
+      </Container>
+      <Copyright />
+    </Fragment>
+  );
+}
+
+export default App;
