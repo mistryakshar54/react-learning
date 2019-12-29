e3b813e4794a86fb0b178d41da7f8f365b5b087d
Akshar Mistry <mistry.aks@gmail.com>
2019-12-29 18:52:18 +0530

    Update App.js

--- mini-projects/hacker-news-clone-pwa/src/App.js
+++ mini-projects/hacker-news-clone-pwa/src/App.js
@@ -4,37 +4,40 @@ import Typography from "@material-ui/core/Typography";
 import AppBar from "@material-ui/core/AppBar";
 import Link from "@material-ui/core/Link";
 import Toolbar from "@material-ui/core/Toolbar";
-
+import { Provider } from "react-redux";
+import store from './store/Store';
 import Layout from './Components/LayoutComponent/Layout';
 function Copyright() {
   return (
+    
     <Typography variant="body2" color="textSecondary" align="center">
       {"Copyright © "}
       <Link color="inherit" href="https://material-ui.com/">
         Your Website
-      </Link>{" "}
+      </Link>
       {new Date().getFullYear()}
-      {"."}
     </Typography>
   );
 }
 
 function App() {
   return (
-    <Fragment>
-      <AppBar position="relative">
-        <Toolbar>
-          <Typography variant="h6" color="inherit" noWrap>
-            Hacker News Clone
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Container maxWidth="lg">
-        <Layout />
-        
-      </Container>
-      <Copyright />
-    </Fragment>
+    <Provider store={store}>
+      <Fragment>
+        <AppBar position="relative">
+          <Toolbar>
+            <Typography variant="h6" color="inherit" noWrap>
+              Hacker News Clone
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Container maxWidth="lg">
+          <Layout />
+          
+        </Container>
+        <Copyright />
+      </Fragment>
+    </Provider>
   );
 }
 
