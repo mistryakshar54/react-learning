9bbddddd39edbf3ebd1e3dacdc39415145af4d28
Akshar Mistry <mistry.aks@gmail.com>
2019-12-31 12:47:53 +0530

    Create a fallback component

---
+++ mini-projects/hacker-news-clone-pwa/src/Components/LayoutComponent/NotFoundComponent/NotFoundComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Typography from "@material-ui/core/Typography";
+
+const NotFoundComponent = ( props )=> {
+    return (
+      <Typography variant="h4" component="h1" gutterBottom>
+        Lost.. Are you ?
+      </Typography>
+    );
+}
+
+export default NotFoundComponent;
