a6708c5940d870fecd7b375e3f621b56ebe475aa
Akshar Mistry <mistry.aks@gmail.com>
2019-12-31 14:03:22 +0530

    Add Lazy loading for routes

--- mini-projects/hacker-news-clone-pwa/src/Components/LayoutComponent/Layout.js
+++ mini-projects/hacker-news-clone-pwa/src/Components/LayoutComponent/Layout.js
@@ -1,20 +1,24 @@
-import React from 'react';
-import NewsComponent from '../NewsComponent/NewsComponent';
-import { makeStyles } from "@material-ui/core/styles";
+import React, { Suspense, lazy } from "react";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 import { Route , Switch } from 'react-router-dom';
-import NotFoundComponent from './NotFoundComponent/NotFoundComponent';
+
+const News = lazy(() => import("../NewsComponent/NewsComponent"));
+const NotFound = lazy(() => import("./NotFoundComponent/NotFoundComponent"));
+
 const Layout = ( props ) => {
     return (
-      <Paper style={ { marginTop:'5%' } } elevation={3}>
-        <Switch>
-          <Route path="/top" exact component={NewsComponent} />
-          <Route path="*"  component={NotFoundComponent} />
-        </Switch>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Create React App v4-beta example
-        </Typography>
+      <Paper style={{ marginTop: "5%" }} elevation={3}>
+         <Suspense fallback={<div>Loading...</div>}>
+         
+            <Switch>
+            <Route path="/top" exact component={News} />
+            <Route path="*" component={NotFound} />
+            </Switch>
+            <Typography variant="h4" component="h1" gutterBottom>
+            Create React App v4-beta example
+            </Typography>
+         </Suspense> 
       </Paper>
     );
 }
