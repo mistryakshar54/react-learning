b6bd905a7431f60dcd11eb47bd8a298ec398a4ee
Akshar Mistry <mistry.aks@gmail.com>
2020-05-27 01:42:02 +0530

    Add input for todo list

--- react-with-TS/simple-todo-ts/src/components/layout/layout.tsx
+++ react-with-TS/simple-todo-ts/src/components/layout/layout.tsx
@@ -1,18 +1,11 @@
-import React, { useContext } from 'react';
-
-import {AppContext} from '../../context/context';
+import React from 'react';
 import TodoList from '../todoList/todoList';
+import TodoInput from "../totoInput/todoInput";
 const Layout = () => {
-    const {appState, addTodo,completeTodo, removeTodo} = useContext(AppContext);
-    console.log("appState", appState);
     return (
         <>
-        <button onClick={ () => {addTodo({ name: "ABC", isComplete: false })} }>ABc</button>
-        <button onClick={ () => {addTodo({ name: "DEF", isComplete: false })} }>DEF</button>
-        <button onClick={ () => {completeTodo({ name: "DEF", isComplete: true })} }>DEF</button>
-        <button onClick={ () => {removeTodo('DEF');} }>Click</button>
         <div>Layout Component Comes Here</div>
-        <div>Input Comes Here</div>
+        <TodoInput />
         <TodoList />
       </>
     );
--- react-with-TS/simple-todo-ts/src/components/totoInput/todoInput.tsx
+++ react-with-TS/simple-todo-ts/src/components/totoInput/todoInput.tsx
@@ -1,3 +1,19 @@
-import * as React from "react";
-const TodoInput = () => {};
+import React, { useContext , useState } from "react";
+import { AppContext } from "../../context/context";
+
+const TodoInput = () => {
+    const {addTodo} = useContext(AppContext);
+    const [ todoName , addTodoName ] = useState('');
+    const handleAddTodo = () =>
+    {
+        addTodo({ name: todoName, isComplete: false });
+        addTodoName('');
+    }
+    return (
+      <div>
+        <input value={todoName} onChange={(e) => addTodoName(e.target.value)} />
+        <button onClick={handleAddTodo}>Add</button>
+      </div>
+    );
+};
 export default TodoInput;
