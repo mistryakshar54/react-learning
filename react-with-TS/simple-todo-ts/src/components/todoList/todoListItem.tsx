5301de04c1016afe70640e62fa0ea563c549d721
Akshar Mistry <mistry.aks@gmail.com>
2020-05-27 23:04:05 +0530

    Add fontawesome icons

--- react-with-TS/simple-todo-ts/src/components/todoList/todoListItem.tsx
+++ react-with-TS/simple-todo-ts/src/components/todoList/todoListItem.tsx
@@ -1,18 +1,24 @@
 import React, { useContext } from "react";
 import { AppContext } from "../../context/context";
 import { todo } from "../../context/types";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
 const TodoListItem = ( props : {todoItem : todo} ) => {
     const {completeTodo, removeTodo} = useContext(AppContext);
     const { todoItem } = props;
     return (
       <div
+        className="card col-6 todoItemCard"
         style={{
-          textDecoration: todoItem.isComplete ? "line-through" : 'none',
+          textDecoration: todoItem.isComplete ? "line-through" : "none",
         }}
       >
-        <div onClick={() => completeTodo(todoItem)}>{todoItem.name}</div>
-        <label onClick={() => removeTodo(todoItem.name)}>X</label>
+        <div onClick={() => completeTodo(todoItem)} className="todoName">{todoItem.name}</div>
+        <FontAwesomeIcon
+          className="removeTodo"
+          onClick={() => removeTodo(todoItem.name)}
+          icon={faTimes}
+        />
       </div>
     );
 };
--- react-with-TS/simple-todo-ts/src/components/totoInput/todoInput.tsx
+++ react-with-TS/simple-todo-ts/src/components/totoInput/todoInput.tsx
@@ -1,5 +1,7 @@
 import React, { useContext , useState } from "react";
 import { AppContext } from "../../context/context";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 const TodoInput = () => {
     const {addTodo} = useContext(AppContext);
@@ -10,9 +12,15 @@ const TodoInput = () => {
         addTodoName('');
     }
     return (
-      <div>
-        <input value={todoName} onChange={(e) => addTodoName(e.target.value)} />
-        <button onClick={handleAddTodo}>Add</button>
+      <div className="col-12 todoInputComponent">
+        <input
+          className="todoInput col-4"
+          value={todoName}
+          onChange={(e) => addTodoName(e.target.value)}
+        />
+        <div onClick={handleAddTodo} className="submitButton">
+          <FontAwesomeIcon className="addIcon" icon={faPlus} />
+        </div>
       </div>
     );
 };
