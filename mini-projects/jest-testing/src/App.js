c7b170211cffa4639e31a9ad3c15541081ad8bf3
Akshar Mistry <mistry.aks@gmail.com>
2020-06-13 20:25:56 +0530

    initialize project

---
+++ mini-projects/jest-testing/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import PostComponent from "./Post/Post";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <PostComponent />
+      </header>
+    </div>
+  );
+}
+
+export default App;
