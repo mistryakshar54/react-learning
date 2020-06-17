5cb3b77f6e69c9b7b8e446863ccbfc7a4d50f7d0
Akshar Mistry <mistry.aks@gmail.com>
2020-06-17 22:22:45 +0530

    Add post component

--- mini-projects/jest-testing/src/Post/Post.js
+++ mini-projects/jest-testing/src/Post/Post.js
@@ -1,28 +1,42 @@
 import React , { useState , useEffect } from 'react';
 import fetchData from '../utls/utils';
 const PostComponent = () => {
-    const [currentPost , setPost] = useState(null);
-    const [errorMsg , setError] = useState('');
-    const [isLoading , setLoading] = useState(true);
-    const fetcher = async() => {
-        const postData = await fetchData();
-        if( postData.status === 200 ){
-          setPost(postData.data);
-        }
-        else{
-          setError(postData.message);
-        }
-        setLoading(false);
-      }
-    useEffect(() => {
-      setLoading(true);
-      fetcher();
-    },[]);
-    return (
-        isLoading ? <h1>Loading</h1> :
-        errorMsg ? <h1>{errorMsg}</h1>:
-        <h1>{currentPost.title}</h1>
-    );
+   const [currentPost, setPost] = useState(null);
+   const [isLoading, setLoading] = useState(true);
+   const [errorMsg, setError] = useState("");
+   const [postNo, setPostNumber] = useState(1);
+
+   useEffect(() => {
+     setLoading(true);
+     const loadData = async () => {
+       const result = await fetchData(postNo);
+       if (result.status === 200) {
+         setPost(result.data);
+       } else {
+         setError(result.message);
+       }
+       setLoading(false);
+     };
+     loadData();
+   }, [postNo]);
+
+   return isLoading ? (
+     <h1 data-testid="loading">Loading</h1>
+   ) : errorMsg ? (
+     <h1 data-testid="error">{errorMsg}</h1>
+   ) : (
+     <div>
+       <h1 data-testid="resolved">{currentPost.title}</h1>
+       <button
+         id="#nextIcon"
+         onClick={() => {
+           setPostNumber(postNo + 1);
+         }}
+       >
+         Next
+       </button>
+     </div>
+   );
 };
 
 export default PostComponent;
