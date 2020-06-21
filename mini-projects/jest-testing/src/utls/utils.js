1507a2ecbb0d32948b250ace8081ee04c72a69df
Akshar Mistry <mistry.aks@gmail.com>
2020-06-22 02:23:46 +0530

    Update CSS

--- mini-projects/jest-testing/src/utls/utils.js
+++ mini-projects/jest-testing/src/utls/utils.js
@@ -9,22 +9,9 @@ const fetchData = async (postId = 1) => {
   if( isNaN( postId ) ){
       return { ...responseObj, status: 500, message: "Invalid postId.." };
   }
-  // const responseProm = new Promise( ( res , rej ) => {
-  //   setTimeout(() => {
-  //     res({
-  //   userId: 1,
-  //   id: 1,
-  //   title:
-  //     "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-  //   body:
-  //     "quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto",
-  // });
-  //   }, 1000);
-  // } )
   const response = await axios.get(
     `https://jsonplaceholder.typicode.com/posts/${postId}`
   );
-  // const dta = await responseProm;
   return { ...responseObj, data: response.data };
 };
 export default fetchData;
