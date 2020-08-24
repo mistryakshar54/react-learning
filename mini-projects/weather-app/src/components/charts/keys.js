e7834f75689e2ae182704967a1c62100649ec5b9
Akshar Mistry <mistry.aks@gmail.com>
2020-08-24 23:05:58 +0530

    Add app data

---
+++ mini-projects/weather-app/src/components/charts/keys.js
@@ -0,0 +1,1 @@
+export const API_KEY = 'eb7a6ea4fe509644946c27378c8d45e2';
---
+++ mini-projects/weather-app/src/components/layout/layout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TempChart from '../charts/TempChart';
+const Layout = () => {
+    return (
+        <div>
+            <h1>Searchbox Comes Here</h1>
+            <h1>Graphs Come here</h1>
+            <TempChart/>
+        </div>
+    );
+}
+export default Layout;
