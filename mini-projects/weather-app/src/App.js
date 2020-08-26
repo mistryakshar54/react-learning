785177ffebc4789153d4b38bea4e118948ef843d
Akshar Mistry <mistry.aks@gmail.com>
2020-08-27 02:26:56 +0530

    Wire up the store

--- mini-projects/weather-app/src/App.js
+++ mini-projects/weather-app/src/App.js
@@ -2,11 +2,16 @@ import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Layout from "./components/layout/layout";
+import store from './store/store';
+import { Provider } from "react-redux";
+
 function App() {
   return (
-    <div className="App">
-      <Layout />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Layout />
+      </div>
+    </Provider>
   );
 }
 
