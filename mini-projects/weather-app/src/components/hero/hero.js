dbc31a8662a919455a4368b11cdedab92f5747e2
Akshar Mistry <mistry.aks@gmail.com>
2020-08-31 20:33:28 +0530

    Add transition for sun

--- mini-projects/weather-app/src/components/hero/hero.js
+++ mini-projects/weather-app/src/components/hero/hero.js
@@ -1,77 +1,104 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Jumbotron from "react-bootstrap/Jumbotron";
 import './hero.css'
 import {connect} from 'react-redux';
-const HeroComponent = ( ) => {
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
+import { CSSTransition } from "react-transition-group";
+
+// const city = {
+//   id: 1253573,
+//   name: "Vadodara",
+//   coord: {
+//     lat: 22.3,
+//     lon: 73.2,
+//   },
+//   main: {
+//     temp: 301.15,
+//     feels_like: 305.97,
+//     temp_min: 301.15,
+//     temp_max: 301.15,
+//     pressure: 1001,
+//     humidity: 94,
+//   },
+//   dt: 1598710172,
+//   wind: {
+//     speed: 4.1,
+//     deg: 180,
+//   },
+//   sys: {
+//     country: "IN",
+//   },
+//   rain: null,
+//   snow: null,
+//   clouds: {
+//     all: 75,
+//   },
+//   weather: [
+//     {
+//       id: 721,
+//       main: "Haze",
+//       description: "haze",
+//       icon: "50n",
+//     },
+//   ],
+// };
+// console.log("city", city);
+
+const HeroComponent = ( {city}) => {
+    const [showObj, setShowObj] = useState(false);
+    useEffect( () => {
+        if(city.name){
+            setShowObj(true);
+        }else{
+            setShowObj(false);
+        }
+    }, [city]);
     const getHeroData = () => {
-       const city = {
-              id: 1253573,
-              name: "Vadodara",
-              coord: {
-                lat: 22.3,
-                lon: 73.2,
-              },
-              main: {
-                temp: 301.15,
-                feels_like: 305.97,
-                temp_min: 301.15,
-                temp_max: 301.15,
-                pressure: 1001,
-                humidity: 94,
-              },
-              dt: 1598710172,
-              wind: {
-                speed: 4.1,
-                deg: 180,
-              },
-              sys: {
-                country: "IN",
-              },
-              rain: null,
-              snow: null,
-              clouds: {
-                all: 75,
-              },
-              weather: [
-                {
-                  id: 721,
-                  main: "Haze",
-                  description: "haze",
-                  icon: "50n",
-                },
-              ],
-        };
-        console.log("city", city);
       if (!city.name) {
         return (
-          <>
+          <div className="container">
             <h1>Welcome to the weather app</h1>
             <p>
               Start searching for a city to show the current weather information
             </p>
-          </>
+          </div>
         );
       }
       else{
           const {  name, dt, timeZoneOffset, main, weather } = city;
+          const date = new Date((dt - timeZoneOffset) * 1000);
+          const mode = (date.getUTCHours() > 12) ? "faSun" : 'faMoon';
+          console.log("mode", mode, date.getUTCHours());
           return (
-            <>
+            <div className="container">
+              {/* <button onClick={ () => setShowObj(true) }>Click</button> */}
+              <CSSTransition
+                in={showObj}
+                timeout={500}
+                classNames="obj"
+                unmountOnExit
+              >
+                <FontAwesomeIcon icon={faSun} className="sun" />
+              </CSSTransition>
               <h1>
                 {name} | {parseInt(main.temp - 273.15)}°C
               </h1>
-              <h3>{new Date((dt - timeZoneOffset) * 1000).toUTCString()}</h3>
+              <h3>{date.toUTCString()}</h3>
               <h4>
                 Feels Like: {parseInt(main.feels_like - 273.15)}°C | Humidity :{" "}
                 {main.humidity} %
               </h4>
-              <h4 style={{textTransform: 'capitalize'}}>{weather.map((item) => item.description)}</h4>
-            </>
+              <h4 style={{ textTransform: "capitalize" }}>
+                {weather.map((item) => item.description)}
+              </h4>
+            </div>
           );
       }
     };
     return (
       <Jumbotron fluid>
-        <div className="container">{getHeroData()}</div>
+        {getHeroData()}
       </Jumbotron>
     );
 }
